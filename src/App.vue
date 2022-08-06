232fbe72fc59b65c6d0213447cb84851ff42ca78
David F <david.fitoussi@decathlon.com>
2022-08-06 15:18:33 +0200

    feat(mvp): mvp code

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,232 @@
 <template>
   <v-app>
   <v-container>
-    <v-flex md12 xs12>
+    <v-container md12 xs12>
       <v-row>
-        <v-col md6>
-          <span>Wallet address - {{walletAddress}} </span>
+        <v-col md3>
+          <span>Wallet address <br> {{ userAddress }} </span>
         </v-col>
-        <v-col md6>
-          <span>Current debt celling - {{debtCelling}}</span>
+        <v-col md3>
+          <span>Manager access <br> <a href='https://discord.gg/KXcMzCTQzS'>https://discord.gg/KXcMzCTQzS</a></span>
         </v-col>
-      </v-row>
-      <v-row v-if='walletAddress'>
-        <v-col md6>
-          <v-text-field v-model='scAddress' label='Smart contract address'></v-text-field>
+        <v-col md3>
+          <span>Best Aavegotchi Guild <br> <a href='https://t.co/R5ww4ZDFHp'>Gotchi FArmy Gang</a></span>
         </v-col>
-        <v-col md6>
-          <v-textarea v-model='abi' label='abi'></v-textarea>
+        <v-col md3>
+          <span>Manager creator <br> <a href='https://twitter.com/soulk__'>https://twitter.com/soulk__</a></span>
         </v-col>
       </v-row>
-      <v-row v-if='walletAddress'>
-        <v-col md1>
-          <v-text-field v-model='amountToBorrow' label='Amount to borrow'></v-text-field>
-        </v-col>
-        <v-col md2>
-          <v-select v-on:change='changeContractData' v-model='selectVault' :items='vaults' label='Choose vault to interact with'
-                    item-text="text"
-                    item-value="value"></v-select>
-        </v-col>
-      </v-row>
-      <v-row v-if='walletAddress'>
-        <v-col md2>
-          <v-btn color='#0082c3' v-on:click='borrowToken'>Borrow</v-btn>
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <v-row v-if='activated'>
+        <v-col md6>
+          <v-card outlined >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-title class="text-h5 mb-1">
+                  Lending authorization
+                </v-list-item-title>
+                <v-list-item-subtitle>Authorize the bot to manage your lendings based on your own settings</v-list-item-subtitle>
+                <v-list-item-subtitle><strong>Your gotchi need to be in your wallet and not borrowed to appears in this list</strong></v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-avatar
+                  tile
+                  size="80"
+              >
+                <v-img
+                    alt="lending"
+                    src="https://app.aavegotchi.com/images/lending/iconMid.svg"
+                ></v-img>
+              </v-list-item-avatar>
+            </v-list-item>
+            <v-data-table
+                v-model="selected"
+                :headers="headers"
+                :items="userGotchis"
+                item-key="id"
+                show-select
+                class="elevation-1"
+            >
+            </v-data-table>
+
+            <v-card-actions>
+              <v-btn
+                  outlined
+                  rounded
+                  text
+                  :disabled='userGotchis.length === 0 || selected.filter(gotchi => gotchi.isLendingOperator).length === selected.length'
+                  v-on:click='authorizeLending'
+              >
+                Authorize
+              </v-btn>
+              <v-btn
+                  outlined
+                  rounded
+                  text
+                  :disabled='userGotchis.length === 0 || selected.filter(gotchi => !gotchi.isLendingOperator).length === selected.length'
+                  v-on:click='revokeLending'
+              >
+                Revoke
+              </v-btn>
+            </v-card-actions>
+          </v-card>
         </v-col>
         <v-col md6>
-          <v-btn color='#0082c3' v-on:click='subscribeSmartContractEvent'>Subscribe smart contract events</v-btn>
-        </v-col>
-        <v-col md6>
-          <v-btn color='#fe5800' v-on:click='unSubscribeSmartContractEvent'>Unsubscribe smart contract events</v-btn>
+          <v-card outlined >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-title class="text-h5 mb-1">
+                  Pet authorization
+                </v-list-item-title>
+                <v-list-item-subtitle>Authorize the bot to take care of your gotchi</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-avatar
+                  tile
+                  size="80"
+              >
+                <v-img
+                    alt="petting"
+                    src="https://wiki.aavegotchi.com/kinship/petgotchi.gif"
+                ></v-img>
+              </v-list-item-avatar>
+            </v-list-item>
+            <v-card-text>
+              <div v-if='isPetOperatorSet'>You already validated the transaction to accept the pet operator ! Good job ! :)</div>
+              <div v-if='!isPetOperatorSet'>You need to accept the transction to let the manager pet your gotchi !</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                  outlined
+                  rounded
+                  text
+                  :disabled='isPetOperatorSet'
+                  v-on:click='authorizePetting'
+              >
+                Authorize
+              </v-btn>
+              <v-btn
+                  outlined
+                  rounded
+                  text
+                  :disabled='!isPetOperatorSet'
+                  v-on:click='revokePetting'
+              >
+                Revoke
+              </v-btn>
+            </v-card-actions>
+          </v-card>
         </v-col>
       </v-row>
-    </v-flex>
+    </v-container>
   </v-container>
   </v-app>
 </template>
 
 <script>
+import axios from 'axios';
 const Web3 = require('web3');
 let web3 = new Web3(Web3.givenProvider);
-import vaultsContracts from "./data/vaultContracts"
+import diamondcontract from "./data/diamondcontract"
 
 export default {
   name: 'App',
   data() {
     return {
-      walletAddress: "",
-      scAddress: vaultsContracts.vaultvGhst.smartContractAddress,
-      abi: JSON.stringify(vaultsContracts.vaultvGhst.abi),
-      subscription: null,
-      amountToBorrow: 0,
-      debtCelling: 0,
-      selectVault: 'vaultvGhst',
-      vaults: [
-        {text :'vaultvGhst', value: 'vaultvGhst'},
-        {text: 'vaultCamWMatic', value: 'vaultCamWMatic'},
-        {text: 'activeSc', value: 'activeSc'}
-      ]
+      selected: [],
+      headers: [
+        { text: 'Id', value: 'id'},
+        { text: 'Name', value: 'name' },
+        { text: 'Kinship', value: 'kinship' },
+        { text: 'BRS', value: 'baseRarityScore' },
+        { text: 'isLendingOperator', value: 'isLendingOperator' }
+      ],
+      operatorAddress: "0xa9575438851A7eFBa37bC35ebE2be558c4bA3055",
+      operatorPanel : {
+        petOperator: true,
+        lendingOperator: true
+      },
+      isPetOperatorSet: false,
+      userAddress: "Not Connected",
+      userGotchis: [],
+      activated: false,
+      aavegotchiContract: new web3.eth.Contract(diamondcontract.abi, diamondcontract.smartContractAddress)
     }
   },
-  async created() {
-    this.debtCelling = await this.getDebtCeiling();
-  },
   async mounted() {
-    console.log(web3);
     let account = await web3.eth.requestAccounts()
     if(account && account.length > 0) {
-      this.walletAddress=account.pop();
+      this.userAddress = account.pop().toLowerCase()
+    }
+    setInterval(this.updateUserAccount, 1000);
+    setInterval(this.watchAavegotchiAccount, 30000);
+  },
+  watch: {
+    userAddress() {
+      this.userGotchis = []
+      this.isPetOperatorSet = false
+      if (this.userAddress.match(/^0x[a-fA-F0-9]{40}$/)) {
+        this.activated = true;
+        this.isPetOperator()
+        this.loadGotchisList()
+      } else {
+        this.activated = false;
+      }
     }
   },
   methods: {
-    async borrowToken() {
-      let contract = new web3.eth.Contract(JSON.parse(this.abi), this.scAddress);
-      let debtCeilling = await contract.methods.getDebtCeiling().call();
-      if(debtCeilling > 0 ) {
-        const vaultId = await contract.methods.tokenOfOwnerByIndex(this.walletAddress, 0).call();
-        console.log("vault id: " + vaultId);
-        console.log(await contract.methods.borrowToken(vaultId, this.amountToBorrow).send({ from: this.walletAddress }), function(error, transactionHash){
-          if(error) {
-            console.log(error);
-          }
-          console.log(transactionHash);
-        });
-      } else {
-        console.log("Debt ceiling still 0  ...");
+    async updateUserAccount() {
+      let account = await web3.eth.requestAccounts()
+      if (account.length > 0 && account[0].toLowerCase() !== this.userAddress) {
+        this.userAddress = account[0].toLowerCase()
       }
     },
-    async getDebtCeiling() {
-      let contract = new web3.eth.Contract(JSON.parse(this.abi), this.scAddress);
-      const debtCeiling = await contract.methods.getDebtCeiling().call();
-      console.log(debtCeiling);
-      return web3.utils.fromWei(debtCeiling);
+    watchAavegotchiAccount() {
+      this.isPetOperator()
+      this.loadGotchisList()
     },
-    async changeContractData(event) {
-      console.log(event);
-      this.scAddress = vaultsContracts[event].smartContractAddress;
-      this.abi = JSON.stringify(vaultsContracts[event].abi);
-      this.debtCelling = await this.getDebtCeiling();
+    async isPetOperator() {
+      this.isPetOperatorSet = await this.aavegotchiContract.methods.isPetOperatorForAll(this.userAddress,
+          this.operatorAddress).call()
+    },
+    isLendingOperator(gotchiId) {
+      return this.aavegotchiContract.methods.isLendingOperator(this.userAddress,
+          this.operatorAddress, gotchiId).call()
+    },
+    authorizeLending() {
+      this.aavegotchiContract.methods.batchSetLendingOperator(this.operatorAddress,this.selected.map(gotchi => [gotchi.id, true])).send({ from: this.userAddress })
+      this.selected = []
+    },
+    revokeLending() {
+      this.aavegotchiContract.methods.batchSetLendingOperator(this.operatorAddress,this.selected.map(gotchi => [gotchi.id, false])).send({ from: this.userAddress })
+      this.selected = []
+    },
+    authorizePetting() {
+      this.aavegotchiContract.methods.setPetOperatorForAll(this.operatorAddress,
+          true).send({ from: this.userAddress })
+    },
+    revokePetting() {
+      this.aavegotchiContract.methods.setPetOperatorForAll(this.operatorAddress,
+          false).send({ from: this.userAddress })
+    },
+    async loadGotchisList() {
+      const query = `query getGotchiFromWallets {
+        aavegotchis(where: {originalOwner_: {id_in: ["${this.userAddress}"]}, lending:null}) {
+          id
+          name
+          kinship
+          baseRarityScore
+        }
+      }`
+      let gotchisOfOwner = (await axios.post("https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-core-matic", { query: query })).data.data?.aavegotchis
+      let userGotchis = []
+      if (gotchisOfOwner !== null && gotchisOfOwner !== undefined ) {
+        for(const gotchi of gotchisOfOwner) {
+          gotchi["isLendingOperator"] = await this.isLendingOperator(gotchi.id)
+          userGotchis.push(gotchi)
+        }
+      }
+      this.userGotchis = userGotchis
     }
   }
 }
--- src/stores/store.js
+++ src/stores/store.js
@@ -5,9 +5,6 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
-    privateKey: null,
     publicAddress: null,
-    smartContractAddress: null,
-    smartContractAbi: ""
   }
 })
