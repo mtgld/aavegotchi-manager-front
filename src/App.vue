254dc92b73ee3e6f887f9ebbdeab5713abcd0a54
David F <david.fitoussi@decathlon.com>
2022-08-22 16:38:22 +0200

    fix(gotchis): remove lending: null

--- src/App.vue
+++ src/App.vue
@@ -412,7 +412,7 @@ export default {
     },
     async loadGotchisList() {
       const query = `query getGotchiFromWallets {
-        aavegotchis(where: {originalOwner_: {id_in: ["${this.userAddress}"]}, lending:null}) {
+        aavegotchis(where: {originalOwner_: {id_in: ["${this.userAddress}"]}}) {
           id
           name
           kinship
